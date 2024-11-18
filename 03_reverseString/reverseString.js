a1b754af42f242cd399d624e073c718c6c72fb2e
GhidBase <grvdylans@gmail.com>
2024-11-17 23:36:05 -0700

    Solve exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let newString = [];
+    for (const letter of input) {
+        newString.unshift(letter);
+    }
+    newString = newString.join('');
+    return newString;
 };
 
 // Do not edit below this line
