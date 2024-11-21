4ef0f254b135185719e95998c2df558a898b22c1
GhidBase <grvdylans@gmail.com>
2024-11-21 14:36:30 -0700

    Finish exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(minInput,maxInput) {
+    if(minInput < 0 || typeof minInput != "number" || typeof maxInput != "number" || !Number.isInteger(minInput) || !Number.isInteger(maxInput)) {
+        return "ERROR";
+    }
 
+    if(minInput > maxInput)
+    {
+        let tempNum = minInput;
+        minInput = maxInput;
+        maxInput = tempNum;
+    }
+    let total = 0;
+    for(i = minInput; i <= maxInput ; i++){
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
