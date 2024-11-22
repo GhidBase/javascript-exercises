04e9ec2b24275c86cb5e200160a9acf6b981f642
GhidBase <grvdylans@gmail.com>
2024-11-21 21:33:39 -0700

    Finish exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(inputTemp) {
+  //(x-32) * (5/9)
+  let conversion = (inputTemp - 32) * (5/9);
+  return (conversion % 1 == 0) ? conversion : Number(conversion.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(inputTemp) {
+  // (x * (9/5) + 32)
+  let conversion = inputTemp * (9/5) + 32;
+  return (conversion % 1 == 0) ? conversion : Number(conversion.toFixed(1));
+
 };
 
 // Do not edit below this line
