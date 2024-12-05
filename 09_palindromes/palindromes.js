5ec8eba1fe9defe96895ce57975649e6ad8f3193
GhidBase <grvdylans@gmail.com>
2024-12-05 14:39:34 -0700

    09 complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let filteredWord = word.toLowerCase().split("").filter(item => /^[a-z0-9]+$/.test(item)).join("")
+    let reversedWord = filteredWord.split('').reverse().join("");
+    return reversedWord == filteredWord;
 };
 
+console.log(palindromes('r3ace3car'));
 // Do not edit below this line
 module.exports = palindromes;
