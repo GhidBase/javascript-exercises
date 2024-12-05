300b8d2e354d0da41c59fb464d79cf05b9ae87da
GhidBase <grvdylans@gmail.com>
2024-12-05 15:01:09 -0700

    10 complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(iteration) {
+    if (iteration == 0)
+        return 0;
+    if (iteration < 0)
+        return "OOPS";
 
+    let lastTwo = [1,0];
+    for(i = 1; i < iteration ; i++) {
+        let nextFib = lastTwo[0]+lastTwo[1];
+        lastTwo[1] = lastTwo[0];
+        lastTwo[0] = nextFib;
+    }
+    return lastTwo[0];
 };
+
+fibonacci(4);
 
 // Do not edit below this line
 module.exports = fibonacci;
