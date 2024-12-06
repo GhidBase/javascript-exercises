6820d66845f674a9278e008f18698d1e4f631ede
GhidBase <grvdylans@gmail.com>
2024-12-06 00:35:05 -0700

    Exercise 12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    arr.sort((a, b) => findAge(a) - findAge(b))
+    console.table(arr)
+    return arr[arr.length-1]
 };
+
+function findAge(person) {
+    let yearOfDeath;
+    if(!person.hasOwnProperty('yearOfDeath')) {
+        yearOfDeath = new Date().getFullYear();
+    }
+    else {
+        yearOfDeath = person.yearOfDeath;
+    }
+    person['age'] = yearOfDeath - person.yearOfBirth;
+    return yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
